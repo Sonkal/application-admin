ac8b28eb01711b4fb608449c1c7f4f8e80766088
Miroslav Novak <miroslav.novak@gmail.com>
2017-07-15 21:54:03 +0200

    initial skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminMainComponent } from './admin-main/admin-main.component';
+import {RouterModule,Routes} from "@angular/router";
+import { AdminOverviewComponent } from './admin-overview/admin-overview.component';
+import { AdminExportComponent } from './admin-export/admin-export.component';
 
+const appRoutes: Routes = [
+  { path: 'overview', component: AdminOverviewComponent },
+  { path: 'export', component: AdminExportComponent },
+
+  { path: '',   redirectTo: '/overview', pathMatch: 'full' }
+  // ,
+  // { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    AdminMainComponent,
+    AdminOverviewComponent,
+    AdminExportComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
